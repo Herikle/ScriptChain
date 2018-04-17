4ff169b5f71515a3d1c609dea2c6a8ba9bb5c448
Herikle Mesquita Freire de Lima <herikle.mesquita@academico.ifrn.edu.br>
2018-04-17 11:29:24 -0300

    Blockchain simples com arrays

--- main.js
+++ main.js
@@ -6,10 +6,41 @@ class Block{
         this.timestamp = timestamp;
         this.data = data;
         this.previousHash = previousHash;
-        this.hash = '';
+        this.hash = this.calculateHash();
     }
 
     calculateHash(){
         return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data).toString());
     }
 }
+
+
+
+class Blockchain{
+    constructor(){
+        this.chain = [this.createGenesisBlock()];
+    }
+
+    createGenesisBlock(){
+        return new Block(0,"01/01/2018","Genesis block","0");
+    }
+
+    getLatesBlock(){
+        return this.chain[this.chain.length -1];
+    }
+
+    addBlock(newBlock){
+        newBlock.previousHash = this.getLatesBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock);
+    }
+}
+
+
+
+let herikleCoin = new Blockchain();
+herikleCoin.addBlock(new Block(1,"17/04/2018",{amount:4}));
+herikleCoin.addBlock(new Block(2,"15/04/2018",{amount:10}));
+herikleCoin.addBlock(new Block(3,"11/04/2018",{amount:17}));
+
+console.log(JSON.stringify(herikleCoin,null,4));
